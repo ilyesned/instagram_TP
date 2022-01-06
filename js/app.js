1463a6b75df2374a435f6f9e5ccfc1a4304e0bbe
Ilyes <ilyesned@hotmail.com>
2022-01-06 16:34:13 +0100

    JS et responsive

--- js/app.js
+++ js/app.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded",()=>{
 
+    let mailUser = document.querySelector("#adressMail");
+    let passwordUser = document.querySelector("#password");
+    
+    let verifyText = document.querySelector(".verify")
+    let submmit_btn = document.querySelector(".btn_connect")
 
-let password = document.querySelector("#adressMail");
-let mail = document.querySelector("#password");
-
-let change_text = document.querySelector(".verif_text")
-
-let submmit = document.querySelector(".subconnect")
-
-
-submmit.addEventListener("click",()=>{
-    alert(password.value)
-    alert(mail.value)
-})
-
-})
+    let  mail_verif =
+        /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+    
+    submmit_btn.addEventListener("click",(e)=>{
+        e.preventDefault();
+        if((passwordUser.value.length>0)&&(mailUser.value.match(mail_verif))){
+            location.href = "./main_page.html";
+            localStorage.setItem("adressemail", mailUser.value);
+    
+        }else
+        {
+            verifyText.classList.add("verifyShow")
+            verifyText.innerText= "Votre adresse mail et votre mot de passe sont incorrects "
+        }
+    })
+})    
